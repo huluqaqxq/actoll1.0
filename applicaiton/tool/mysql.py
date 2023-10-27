0d3f3e8d521e2160c488c869a985768859a94c45
black-dragon <74292567+huluqaqxq@users.noreply.github.com>
2023-10-27 10:01:06 +0800

    Update mysql.py
    if not result  is not use

--- applicaiton/tool/mysql.py
+++ applicaiton/tool/mysql.py
@@ -10,6 +10,6 @@ def mysql_init(host, user, password, database):
 def select(log, cursor, sql):
     cursor.execute(sql)
     result = cursor.fetchall()
-    if not result:
+    if len(result) == 0:
         return []
     return pd.DataFrame(result, columns=[column[0] for column in cursor.description])
